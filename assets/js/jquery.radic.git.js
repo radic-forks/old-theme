12bab498d914cc8feea2ab796baf152ad5c6c50b
Robin Radic <robin@radic.nl>
2014-11-03 10:27:58 +0100

    new stuff

---
+++ assets/js/jquery.radic.git.js
@@ -0,0 +1,12 @@
+(function () {
+    $.Radic = function () {
+
+    };
+
+    $.Radic.defaults = {};
+
+    $.Radic.prototype.robin = function () {
+        console.log('The greatest, the best');
+    };
+
+}).call();
